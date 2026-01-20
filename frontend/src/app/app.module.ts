49c4ce7e5173f8d0436a428d49574495ec902bbf
Sen Lin <hqfosao828@hotmail.com>
2026-01-21 01:14:20 +0800

    Update app.module.ts with new module structure

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,120 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { ProductService } from './services/product.service';
-import { Router, RouterModule, Routes } from '@angular/router';
-import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
-import { SearchComponent } from './components/search/search.component';
-import { ProductDetailsComponent } from './components/product-details/product-details.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { CartStatusComponent } from './components/cart-status/cart-status.component';
-import { CartDetailsComponent } from './components/cart-details/cart-details.component';
-import { CheckoutComponent } from './components/checkout/checkout.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './components/login/login.component';
-import { LoginStatusComponent } from './components/login-status/login-status.component';
-
-import appConfig from './config/app-config';
-
-import {
-  OKTA_CONFIG,
-  OktaAuthModule,
-  OktaCallbackComponent,
-  OktaAuthGuard,
-} from '@okta/okta-angular';
-import { MembersPageComponent } from './components/members-page/members-page.component';
-import { OrderHistoryComponent } from './components/order-history/order-history.component';
-import { AuthInterceptorService } from './services/auth-interceptor.service';
-import { EduHomeComponent } from './components/edu-home/edu-home.component';
-import { EduSimulationComponent } from './components/edu-simulation/edu-simulation.component';
-import { EduMethodsComponent } from './components/edu-methods/edu-methods.component';
-import { EduRiskAlertsComponent } from './components/edu-risk-alerts/edu-risk-alerts.component';
-import { EduRoboticsComponent } from './components/edu-robotics/edu-robotics.component';
-import { EduLearnComponent } from './components/edu-learn/edu-learn.component';
-
-const oktaConfig = Object.assign(
-  {
-    onAuthRequired: (oktaAuth, injector) => {
-      const router = injector.get(Router);
-
-      router.navigate(['/login']);
-    },
-  },
-  appConfig.oidc
-);
-
-const routes: Routes = [
-  {
-    path: 'members',
-    component: MembersPageComponent,
-    canActivate: [OktaAuthGuard],
-  },
-  {
-    path: 'order-history',
-    component: OrderHistoryComponent,
-    canActivate: [OktaAuthGuard],
-  },
-  { path: 'login/callback', component: OktaCallbackComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'checkout', component: CheckoutComponent },
-  { path: 'cart-details', component: CartDetailsComponent },
-  { path: 'products/:id', component: ProductDetailsComponent },
-  { path: 'search/:keyword', component: ProductListComponent },
-  { path: 'category/:id', component: ProductListComponent },
-  { path: 'category', component: ProductListComponent },
-  { path: 'products', component: ProductListComponent },
-  { path: 'edu/home', component: EduHomeComponent },
-  { path: 'edu/simulation', component: EduSimulationComponent },
-  { path: 'edu/methods', component: EduMethodsComponent },
-  { path: 'edu/risk-alerts', component: EduRiskAlertsComponent },
-  { path: 'edu/robotics', component: EduRoboticsComponent },
-  { path: 'edu/learn', component: EduLearnComponent },
-  { path: 'edu', redirectTo: '/edu/home', pathMatch: 'full' },
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: '**', redirectTo: '/products', pathMatch: 'full' },
-];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    ProductCategoryMenuComponent,
-    SearchComponent,
-    ProductDetailsComponent,
-    CartStatusComponent,
-    CartDetailsComponent,
-    CheckoutComponent,
-    LoginComponent,
-    LoginStatusComponent,
-    MembersPageComponent,
-    OrderHistoryComponent,
-    EduHomeComponent,
-    EduSimulationComponent,
-    EduMethodsComponent,
-    EduRiskAlertsComponent,
-    EduRoboticsComponent,
-    EduLearnComponent,
-  ],
-  imports: [
-    RouterModule.forRoot(routes),
-    BrowserModule,
-    HttpClientModule,
-    NgbModule,
-    ReactiveFormsModule,
-    OktaAuthModule,
-  ],
-  providers: [
-    ProductService,
-    { provide: OKTA_CONFIG, useValue: oktaConfig },
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
